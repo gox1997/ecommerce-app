1a2dbed0278962b2705ae24d30b111f0dde1f8d0
gox1997 <goran.skaljac97@gmail.com>
2026-01-24 17:31:41 +0100

    created CartPage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -175,10 +175,11 @@ function CartPage() {
                                 </span>
                             </div>
                         </div>
-
-                        <button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition-colors mb-3">
-                            Proceed to Checkout
-                        </button>
+                        <Link to="/checkout">
+                            <button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition-colors mb-3">
+                                Proceed to Checkout
+                            </button>
+                        </Link>
 
                         <Link
                             to="/"
